551f0a24a515777e920d20d2ccd97876ca0809e8
furikake6000 <ewave63@keio.jp>
2019-08-31 13:44:12 +0900

    initial commit

---
+++ Assets/Scripts/WebView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebView : MonoBehaviour {
+
+	WebViewObject wb;
+
+	// Use this for initialization
+	void Start () {
+		wb = gameObject.GetComponent<WebViewObject>();
+
+		wb.Init((msg) => {
+            Debug.Log(msg);
+        });
+        wb.LoadURL("https://shinchoku.net");
+        // 中央に配置
+        wb.SetMargins(0, 0, 0, 0);
+        wb.SetVisibility(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
